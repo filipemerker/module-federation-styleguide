fe6dea4b7a413c1dc5a9d96bb090d0002e779636
Filipe Merker <filipesmerker@gmail.com>
2020-08-08 18:24:29 +0200

    Change dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,42 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index",
-  mode: "development",
+  entry: './src/index',
+  mode: 'development',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'docs'),
     port: 3006,
   },
   output: {
-    publicPath: "http://localhost:3006/",
+    publicPath: 'http://localhost:3006/',
+    path: path.join(__dirname, 'docs')
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-react"],
+          presets: ['@babel/preset-react'],
         },
       },
     ],
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "styleguide",
-      library: { type: "var", name: "styleguide" },
-      filename: "remoteEntry.js",
+      name: 'styleguide',
+      library: { type: 'var', name: 'styleguide' },
+      filename: 'remoteEntry.js',
       exposes: {
-        "./Button": "./src/Button",
+        './Button': './src/Button',
       },
-      shared: ["react", "react-dom"],
+      shared: ['react', 'react-dom'],
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
