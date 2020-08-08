dccdce4c56f610541be019181797e5a9b39bb4d5
Filipe Merker <filipesmerker@gmail.com>
2020-08-08 19:13:07 +0200

    Update button

--- docs/921.js
+++ docs/921.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp_basic_host_remote_app2=window.webpackJsonp_basic_host_remote_app2||[]).push([[921],{921:(t,e,o)=>{"use strict";o.r(e),o.d(e,{default:()=>s});var p=o(104),n=o.n(p);const s=()=>n().createElement("button",null,"App 2 Button")}}]);
+(window.webpackJsonp_basic_host_remote_app2=window.webpackJsonp_basic_host_remote_app2||[]).push([[921],{921:(t,e,o)=>{"use strict";o.r(e),o.d(e,{default:()=>a});var n=o(104),s=o.n(n);const a=()=>s().createElement("button",null,"New Button!")}}]);
